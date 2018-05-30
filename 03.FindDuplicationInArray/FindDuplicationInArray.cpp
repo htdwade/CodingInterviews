97a45175e11b9dcc2b249ae97228249a1761f302
htdwade <1046217644@qq.com>
2018-05-30 20:57:31 +0800

    updated

--- 03.FindDuplicationInArray/FindDuplicationInArray.cpp
+++ 03.FindDuplicationInArray/FindDuplicationInArray.cpp
@@ -2,6 +2,11 @@
 
 剑指Offer
 面试题3：数组中重复的数字
+题目一：找出数组中重复的数字
+在一个长度为n的数组里的所有数字都在0~n-1的范围内。数组中某些数字是重复的，但不知道有几个数字重复了，
+也不知道每个数字重复了几次。请找出数组中任意一个重复的数字。 例如，如果输入长度为7的数组{2,3,1,0,2,5,3}，
+那么对应的输出是重复的数字2或者3。
+
 题目二：不修改数组找出数组中重复的数字
 在一个长度为n+1的数组里的所有数字都在1~n的范围内，所以数组中至
 少有一个数字是重复的。请找出数组中任意一个重复的数字，但不能修改输入的
@@ -14,7 +19,40 @@
 
 using namespace std;
 
+// 题目一
+// 参数:
+//        numbers:     一个整数数组
+//        length:      数组的长度
+//        duplication: (输出) 数组中的一个重复的数字
+// 返回值:             
+//        true  - 输入有效，并且数组中存在重复的数字
+//        false - 输入无效，或者数组中没有重复的数字
+bool duplicate(int numbers[], int length, int* duplication)
+{
+	if (numbers == nullptr || length <= 0)
+		return false;
+	for (int i = 0; i < length; ++i)
+	{
+		if (numbers[i] < 0 || numbers[i] > length - 1)
+			return false;
+	}
+	for (int i = 0; i < length; ++i)
+	{
+		while (numbers[i] != i)
+		{
+			if (numbers[i] == numbers[numbers[i]])
+			{
+				*duplication = numbers[i];
+				return true;
+			}
+			swap(numbers[i], numbers[numbers[i]]);
+		}
+	}
+	return false;
+}
+
 int countRange(const int* numbers, int length, int start, int end);
+// 题目二
 // 参数:
 //        numbers:     一个整数数组
 //        length:      数组的长度
@@ -59,8 +97,15 @@ int countRange(const int* numbers, int length, int start, int end)
 
 int main()
 {
-	int numbers[] = { 2, 3, 5, 4, 3, 2, 6, 7 };
-	int length = sizeof(numbers) / sizeof(numbers[0]);
-	cout << getDuplication(numbers, length) << endl;
+	int numbers1[] = { 2, 3, 1, 0, 2, 5, 3 };
+	int *duplication = new int(-1);
+	int length1 = sizeof(numbers1) / sizeof(numbers1[0]);
+	duplicate(numbers1, length1, duplication);
+	cout << *duplication << endl;
+
+	int numbers2[] = { 2, 3, 5, 4, 3, 2, 6, 7 };
+	int length2 = sizeof(numbers2) / sizeof(numbers2[0]);
+	cout << getDuplication(numbers2, length2) << endl;
+
 	return 0;
 }
---
+++ 09.QueueWithTwoStacks/QueueWithTwoStacks.cpp
@@ -0,0 +1,91 @@
+/*
+
+剑指Offer
+面试题9：用两个栈实现队列
+题目：用两个栈实现一个队列。队列的声明如下，请实现它的两个函数appendTail
+和deleteHead，分别完成在队列尾部插入结点和在队列头部删除结点的功能。
+
+*/
+
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+template <typename T> class CQueue
+{
+public:
+	CQueue(void);
+	~CQueue(void);
+
+	// 在队列末尾添加一个结点
+	void appendTail(const T& node);
+
+	// 删除队列的头结点
+	T deleteHead();
+
+private:
+	stack<T> stack1;
+	stack<T> stack2;
+};
+
+template <typename T> CQueue<T>::CQueue(void)
+{
+}
+
+template <typename T> CQueue<T>::~CQueue(void)
+{
+}
+
+template<typename T> void CQueue<T>::appendTail(const T& element)
+{
+	stack1.push(element);
+}
+
+template<typename T> T CQueue<T>::deleteHead()
+{
+	if (stack2.size() <= 0)
+	{
+		while (stack1.size()>0)
+		{
+			T& data = stack1.top();
+			stack1.pop();
+			stack2.push(data);
+		}
+	}
+
+	if (stack2.size() == 0)
+		throw new exception("queue is empty");
+
+	T head = stack2.top();
+	stack2.pop();
+
+	return head;
+}
+
+int main()
+{
+	CQueue<char> queue;
+
+	queue.appendTail('a');
+	queue.appendTail('b');
+	queue.appendTail('c');
+
+	char head = queue.deleteHead();
+	cout << head << endl;
+
+	head = queue.deleteHead();
+	cout << head << endl;
+
+	queue.appendTail('d');
+	head = queue.deleteHead();
+	cout << head << endl;
+
+	queue.appendTail('e');
+	head = queue.deleteHead();
+	cout << head << endl;
+
+	head = queue.deleteHead();
+	cout << head << endl;
+	return 0;
+}
