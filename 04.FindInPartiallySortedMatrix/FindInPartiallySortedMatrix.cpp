9239d17c87259b6c3d221f406bff48411bc2b8ce
htdwade <1046217644@qq.com>
2018-11-08 15:43:20 +0800

    updated

--- 04.FindInPartiallySortedMatrix/FindInPartiallySortedMatrix.cpp
+++ 04.FindInPartiallySortedMatrix/FindInPartiallySortedMatrix.cpp
@@ -14,21 +14,21 @@
 using namespace std;
 
 bool Find(int target, vector<vector<int>> array) {
-        if(array.size() == 0 || array[0].size() == 0)
-            return false;
-        int rows = array.size();
-        int columns = array[0].size();
-        int row = 0;
-        int column = columns - 1;
-        while(row < rows && column >= 0){
-            if(array[row][column] == target)
-                return true;
-            else if(array[row][column] > target)
-                column--;
-            else
-                row++;
-        }
+    if(array.size() == 0 || array[0].size() == 0)
         return false;
+    int rows = array.size();
+    int columns = array[0].size();
+    int row = 0;
+    int column = columns - 1;
+    while(row < rows && column >= 0){
+        if(array[row][column] == target)
+            return true;
+        else if(array[row][column] > target)
+            column--;
+        else
+            row++;
+    }
+    return false;
 }
 
 int main()
--- 05.ReplaceSpaces/ReplaceSpaces.cpp
+++ 05.ReplaceSpaces/ReplaceSpaces.cpp
@@ -1,4 +1,4 @@
-/* 
+/*
 
 剑指Offer
 面试题5：二维数组中的查找
@@ -12,45 +12,39 @@
 
 using namespace std;
 
-void ReplaceBlank(char str[], int length)
-{
-	if (str == nullptr && length <= 0)
-		return;
-	int originalLength = 0;
-	int numberOfBlank = 0;
-	int i = 0;
-	while (str[i] != '\0')
-	{
-		++originalLength;
-		if (str[i] == ' ')
-			++numberOfBlank;
-		++i;
-	}
-	int newLength = originalLength + numberOfBlank * 2;
-	if (newLength >= length)
-		return;
-	int indexOfOriginal = originalLength;
-	int indexOfNew = newLength;
-	while (indexOfOriginal >= 0 && indexOfNew > indexOfOriginal)
-	{
-		if (str[indexOfOriginal] == ' ')
-		{
-			str[indexOfNew--] = '0';
-			str[indexOfNew--] = '2';
-			str[indexOfNew--] = '%';
-		}
-		else
-		{
-			str[indexOfNew--] = str[indexOfOriginal];
-		}
-		--indexOfOriginal;
-	}
+void replaceSpace(char *str,int length) {
+    if(str == nullptr || length <= 0)
+        return;
+    int originLength = 0;
+    int count = 0;
+    char *p = str;
+    while(*p != '\0'){
+        if(*p == ' ')
+            count++;
+        originLength++;
+        p++;
+    }
+    int newLength = originLength + 2 * count;
+    if(newLength >= length)
+        return;
+    int originIndex = originLength;
+    int newIndex = newLength;
+    while(originIndex >=0 && newIndex > originIndex){
+        if(str[originIndex] == ' '){
+            str[newIndex--] = '0';
+            str[newIndex--] = '2';
+            str[newIndex--] = '%';
+        }
+        else
+            str[newIndex--] = str[originIndex];
+        originIndex--;
+    }
 }
 
 int main()
-{	
+{
 	char str[20] = "We are happy.";
-	ReplaceBlank(str, 20);
+	replaceSpace(str, 20);
 	printf("%s\n", str);
 	return 0;
 }
