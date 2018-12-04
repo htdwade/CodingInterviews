634e802d8bd01ff69b7a5be90755c9fcb70f72a7
htdwade <1046217644@qq.com>
2018-12-04 20:55:01 +0800

    updated

--- 14.CuttingRope/CuttingRope.cpp
+++ 14.CuttingRope/CuttingRope.cpp
@@ -10,6 +10,8 @@
 */
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -17,51 +19,25 @@ using namespace std;
 int maxProductAfterCutting_solution1(int length) {
 	if (length < 2)
 		return 0;
-	if (length == 2)
-		return 1;
-	if (length == 3)
-		return 2;
-	int* products = new int[length + 1];
-	products[0] = 0;
-	products[1] = 1;
-	products[2] = 2;
-	products[3] = 3;
-
-	int max = 0;
-	for (int i = 4; i <= length; i++) {
-		max = 0;
-		for (int j = 1; j <= i / 2; j++) {
-			int product = products[j] * products[i - j];
-			if (max < product)
-				max = product;
-			products[i] = max;
-		}
-	}
-	max = products[length];
-	delete[] products;
-	return max;
+	vector<int> dp(length + 1, 0);
+	dp[1] = 1;
+	for (int i = 2; i <= length; i++)
+		for (int j = 1; j <= i / 2; j++)
+			dp[i] = max(dp[i], max(j, dp[j]) * max(i - j, dp[i - j]));
+	return dp[length];
 }
 
 //贪婪算法
 int maxProductAfterCutting_solution2(int length) {
-	if (length < 2)
-		return 0;
-	if (length == 2)
-		return 1;
-	if (length == 3)
-		return 2;
-
-	//尽可能多地剪去长度为3的绳子段
-	int timesOf3 = length / 3;
-
-	//当绳子最后剩下的长度为4的时候，不能再剪去长度为3的绳子段
-	//此时更好的方法是把绳子剪成长度为2的两段，因为2 * 2 > 3 * 1
-	if (length - timesOf3 * 3 == 1)
-		timesOf3 -= 1;
-
-	int timesOf2 = (length - timesOf3 * 3) / 2;
-
-	return (int)(pow(3, timesOf3)) * (int)(pow(2, timesOf2));
+	if (length <= 3)
+		return 1 * (length - 1);
+	int res = 1;
+	while (length > 4) {
+		res *= 3;
+		length -= 3;
+	}
+	res *= length;
+	return res;
 }
 
 int main()
--- 15.NumberOf1InBinary/NumberOf1InBinary.cpp
+++ 15.NumberOf1InBinary/NumberOf1InBinary.cpp
@@ -19,23 +19,19 @@ int NumberOf1_Solution1(int n)
 	{
 		if (n & flag)
 			count++;
-
 		flag = flag << 1;
 	}
-
 	return count;
 }
 
 int NumberOf1_Solution2(int n)
 {
 	int count = 0;
-
 	while (n)
 	{
 		++count;
 		n = (n - 1) & n;
 	}
-
 	return count;
 }
 
