57ac7d18c906a70a1004129bea322a4c822881dd
htdwade <1046217644@qq.com>
2018-12-04 16:19:29 +0800

    updated

--- 11.MinNumberInRotatedArray/MinNumberInRotatedArray.cpp
+++ 11.MinNumberInRotatedArray/MinNumberInRotatedArray.cpp
@@ -9,64 +9,29 @@
 */
 
 #include <iostream>
-#include <exception>
+#include <vector>
 
 using namespace std;
 
-int MinInOrder(int* numbers, int index1, int index2);
-
-int Min(int* numbers, int length)
-{
-	if (numbers == nullptr || length <= 0)
-		throw new exception("Invalid parameters");
-
-	int index1 = 0;
-	int index2 = length - 1;
-	int indexMid = index1;
-	while (numbers[index1] >= numbers[index2])
-	{
-		// 如果index1和index2指向相邻的两个数，
-		// 则index1指向第一个递增子数组的最后一个数字，
-		// index2指向第二个子数组的第一个数字，也就是数组中的最小数字
-		if (index2 - index1 == 1)
-		{
-			indexMid = index2;
-			break;
-		}
-
-		// 如果下标为index1、index2和indexMid指向的三个数字相等，
-		// 则只能顺序查找
-		indexMid = (index1 + index2) / 2;
-		if (numbers[index1] == numbers[index2] && numbers[indexMid] == numbers[index1])
-			return MinInOrder(numbers, index1, index2);
-
-		// 缩小查找范围
-		if (numbers[indexMid] >= numbers[index1])
-			index1 = indexMid;
-		else if (numbers[indexMid] <= numbers[index2])
-			index2 = indexMid;
-	}
-
-	return numbers[indexMid];
+int minNumberInRotateArray(vector<int> rotateArray) {
+    if(rotateArray.size() == 0)
+        return 0;
+    int start =0, end = rotateArray.size() - 1;
+    while(start < end){
+        int mid = start + (end - start) / 2;
+        if(rotateArray[mid] == rotateArray[end])
+            end--;
+        else if(rotateArray[mid] < rotateArray[end])
+            end = mid;
+        else
+            start = mid + 1;
+    }
+    return rotateArray[start];
 }
-
-int MinInOrder(int* numbers, int index1, int index2)
-{
-	int result = numbers[index1];
-	for (int i = index1 + 1; i <= index2; ++i)
-	{
-		if (result > numbers[i])
-			result = numbers[i];
-	}
-
-	return result;
-}
-
 
 int main()
 {
-	int numbers[] = { 3, 4, 5, 1, 2 };
-	int length = sizeof(numbers) / sizeof(numbers[0]);
-	cout << Min(numbers, length) << endl;
+	vector<int> nums = { 3, 4, 5, 1, 2 };
+	cout << minNumberInRotateArray(nums) << endl;
 	return 0;
 }
