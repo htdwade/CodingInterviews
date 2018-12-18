0ecb1a731b9a21848adb66e52d5fdb0a0e724119
htdwade <1046217644@qq.com>
2018-12-18 16:37:35 +0800

    updated

--- 20.NumericStrings/NumericStrings.cpp
+++ 20.NumericStrings/NumericStrings.cpp
@@ -12,75 +12,56 @@
 #include <iostream>
 #include <string>
 #include <regex>
+#include <vector>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution {
 public:
 	//正则表达式
-	bool isNumeric(const char* str)
-	{
-		if (str == nullptr)
+	bool isNumber(string s) {
+		s.erase(0, s.find_first_not_of(" "));
+		s.erase(s.find_last_not_of(" ") + 1);
+		if (s.empty())
 			return false;
-		regex pattern("[+-]?\\d*(\\.\\d+)?([eE][+-]?\\d+)?");
-		return regex_match(str, pattern);
+		regex pattern("[+-]?((\\d+(\\.\\d*)?)|(\\.\\d+))([Ee][+-]?\\d+)?");
+		return regex_match(s, pattern);
 	}
 
-	// 常规扫描解法
-	// 数字的格式可以用A[.[B]][e|EC]或者.B[e|EC]表示，其中A和C都是
-	// 整数（可以有正负号，也可以没有），而B是一个无符号整数
-	bool isNumeric2(const char* str)
-	{
-		if (str == nullptr)
-			return false;
-
-		bool numeric = scanInteger(str);
-
-		// 如果出现'.'，接下来是数字的小数部分
-		if (*str == '.')
-		{
-			++str;
-
-			// 下面一行代码用||的原因：
-			// 1. 小数可以没有整数部分，例如.123等于0.123；
-			// 2. 小数点后面可以没有数字，例如233.等于233.0；
-			// 3. 当然小数点前面和后面可以有数字，例如233.666
-			numeric = scanUnsignedInteger(str) || numeric;
+	// DFA
+	bool isNumber2(string s) {
+		vector<unordered_map<string, int>> state = { {},
+													{{"blank", 1}, {"sign", 2}, {"digit", 3}, {".", 4}},
+													{{"digit", 3}, {".", 4}},
+													{{"blank", 9}, {"digit", 3}, {"exp", 6}, {".", 5}},
+													{{"digit", 5}},
+													{{"digit", 5}, {"exp", 6}, {"blank", 9}},
+													{{"sign", 7}, {"digit", 8}},
+													{{"digit", 8}},
+													{{"digit", 8}, {"blank", 9}},
+													{{"blank", 9}}
+		};
+		int curState = 1;
+		for (auto c : s) {
+			string t = "";
+			if (c == ' ')
+				t = "blank";
+			if (c == '+' || c == '-')
+				t = "sign";
+			if (c >= '0' && c <= '9')
+				t = "digit";
+			if (c == '.')
+				t = ".";
+			if (c == 'e' || c == 'E')
+				t = "exp";
+			if (state[curState].find(t) == state[curState].end())
+				return false;
+			curState = state[curState][t];
 		}
-
-		// 如果出现'e'或者'E'，接下来跟着的是数字的指数部分
-		if (*str == 'e' || *str == 'E')
-		{
-			++str;
-
-			// 下面一行代码用&&的原因：
-			// 1. 当e或E前面没有数字时，整个字符串不能表示数字，例如.e1、e1；
-			// 2. 当e或E后面没有整数时，整个字符串不能表示数字，例如12e、12e+5.4
-			numeric = numeric && scanInteger(str);
-		}
-
-		return numeric && *str == '\0';
-	}
-
-private:
-	bool scanUnsignedInteger(const char*& str)
-	{
-		int count = 0;
-		while (*str != '\0' && *str >= '0' && *str <= '9') {
-			++str;
-			++count;
-		}
-
-		// 当str中存在若干0-9的数字时，返回true
-		return count > 0;
-	}
-
-	// 整数的格式可以用[+|-]B表示, 其中B为无符号整数
-	bool scanInteger(const char*& str)
-	{
-		if (*str == '+' || *str == '-')
-			++str;
-		return scanUnsignedInteger(str);
+		if (curState == 3 || curState == 5 || curState == 8 || curState == 9)
+			return true;
+		return false;
 	}
 
 };
@@ -88,15 +69,35 @@ private:
 int main()
 {
 	Solution solution;
-	cout << solution.isNumeric("+100") << endl;
-	cout << solution.isNumeric("5e2") << endl;
-	cout << solution.isNumeric("-123") << endl;
-	cout << solution.isNumeric("3.1416") << endl;
-	cout << solution.isNumeric("-1E-16") << endl;
-	cout << solution.isNumeric("12e") << endl;
-	cout << solution.isNumeric("1a3.14") << endl;
-	cout << solution.isNumeric("1.2.3") << endl;
-	cout << solution.isNumeric("+-5") << endl;
-	cout << solution.isNumeric("12e+5.4") << endl;
+	cout << "正则:" << solution.isNumber("+100") << endl;
+	cout << "DFA:" << solution.isNumber2("+100") << endl;
+	cout << '\n';
+	cout << "正则:" << solution.isNumber("5e2") << endl;
+	cout << "DFA:" << solution.isNumber2("5e2") << endl;
+	cout << '\n';
+	cout << "正则:" << solution.isNumber("-123") << endl;
+	cout << "DFA:" << solution.isNumber2("-123") << endl;
+	cout << '\n';
+	cout << "正则:" << solution.isNumber("3.1416") << endl;
+	cout << "DFA:" << solution.isNumber2("3.1416") << endl;
+	cout << '\n';
+	cout << "正则:" << solution.isNumber("-1E-16") << endl;
+	cout << "DFA:" << solution.isNumber2("-1E-16") << endl;
+	cout << '\n';
+	cout << "正则:" << solution.isNumber("12e") << endl;
+	cout << "DFA:" << solution.isNumber2("12e") << endl;
+	cout << '\n';
+	cout << "正则:" << solution.isNumber("1a3.14") << endl;
+	cout << "DFA:" << solution.isNumber2("1a3.14") << endl;
+	cout << '\n';
+	cout << "正则:" << solution.isNumber("1.2.3") << endl;
+	cout << "DFA:" << solution.isNumber2("1.2.3") << endl;
+	cout << '\n';
+	cout << "正则:" << solution.isNumber("+-5") << endl;
+	cout << "DFA:" << solution.isNumber2("+-5") << endl;
+	cout << '\n';
+	cout << "正则:" << solution.isNumber("12e+5.4") << endl;
+	cout << "DFA:" << solution.isNumber2("12e+5.4") << endl;
+	cout << '\n';
 	return 0;
 }
